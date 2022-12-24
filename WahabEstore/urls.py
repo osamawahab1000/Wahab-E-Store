d906e9e8fac5b2b258c9b852e638abd9208d10b0
osamawahab1000 <02-134182-099@student.bahria.edu.pk>
2022-12-24 19:34:49 +0500

    setting up media folder for images and videos

--- WahabEstore/urls.py
+++ WahabEstore/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+import django.urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include("shop.urls")),
-    path('blog/', include("blog.urls")),
-]
+    django.urls.path('admin/', admin.site.urls),
+    django.urls.path('shop/', django.urls.include("shop.urls")),
+    django.urls.path('blog/', django.urls.include("blog.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
