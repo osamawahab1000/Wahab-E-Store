a2446ed0c0057fae3332925fb7baa2b044bb70d8
osamawahab1000 <02-134182-099@student.bahria.edu.pk>
2022-12-28 22:04:41 +0500

    Use logic for showing products

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,17 @@
+from math import ceil
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    Products = Product.objects.all()
+    print(Products)
+    n = len(Products)
+    no_of_Slides = n//4 + ceil((n/4)-(n//4))
+    dict_for_product = {'no_of_Slides':no_of_Slides, 'range': range(1, no_of_Slides), 'product': Products}
+    return render(request, 'shop/index.html', dict_for_product)
 
 def about(request):
     return render(request, 'shop/About.html')
