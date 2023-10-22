93b689b6e204d50409eb2d5a0faf64a3200ff3c4
osamawahab1000 <02-134182-099@student.bahria.edu.pk>
2023-10-23 01:46:08 +0500

    lastest work 23 oct 2023

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, default="")
+    New_arrival = 'New Arrival'
+    Computer_Accessires = 'Computer Accessires'
+    Watches = 'Watch'
+    Mobiles = 'Mobile'
+    Laptops = 'Laoptop'
+    categories = [(New_arrival, 'New-Arrival'), (Computer_Accessires, 'Computer Accessires'), (Watches, 'Watch'),
+                  (Mobiles, 'Mobile'), (Laptops, 'Laptop')]
+    category = models.CharField(max_length=20, choices=categories, default=New_arrival)
     Subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     image = models.ImageField(upload_to="shop/images", default="")
--- shop/views.py
+++ shop/views.py
@@ -4,30 +4,46 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 
+
 # Create your views here.
 def index(request):
     Products = Product.objects.all()
     print(Products)
     n = len(Products)
-    no_of_Slides = n//4 + ceil((n/4)-(n//4))
-    dict_for_product = {'no_of_Slides':no_of_Slides, 'range': range(1, no_of_Slides), 'product': Products}
+    no_of_Slides = n // 4 + ceil((n / 4) - (n // 4))
+    # Showing_all_categories = Products.values('category')
+    # print(Showing_all_categories)
+    # for i in Showing_all_categories:
+    #     print(i)
+
+    categories = Product.objects.values_list('category', flat=True).distinct()
+
+    print(categories)
+
+    dict_for_product = {'no_of_Slides': no_of_Slides, 'range': range(1, no_of_Slides), 'product': Products,
+                        'category': categories}
     return render(request, 'shop/index.html', dict_for_product)
+
 
 def about(request):
     return render(request, 'shop/About.html')
 
+
 def contact(request):
     return HttpResponse("We are at Contact")
+
 
 def tracker(request):
     return HttpResponse("We are at Tracker")
 
+
 def search(request):
     return HttpResponse("We are at Search")
+
 
 def productview(request):
     return HttpResponse("We are at Product View")
 
+
 def checkout(request):
     return HttpResponse("We are at Checkout")
-
